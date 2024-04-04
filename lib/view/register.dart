e9d24bc36a7959a45562d269b83d2e265a4d99e8
Azdiad <azadaduvannic7@gmail.com>
2024-04-04 10:59:39 +0530

    fixedsome

--- lib/view/register.dart
+++ lib/view/register.dart
@@ -1,53 +1,14 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_application_1/controller/register_controller.dart';
-// import 'package:provider/provider.dart';
-
-// class RegisterPage extends StatelessWidget {
-//   const RegisterPage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final p = Provider.of<RegisterController>(context, listen: false);
-//     return Scaffold(
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           TextFormField(
-//             controller: p.emailController,
-//             decoration: InputDecoration(labelText: 'email'),
-//           ),
-//           TextFormField(
-//             controller: p.usernameController,
-//             decoration: InputDecoration(labelText: 'username'),
-//           ),
-//           TextFormField(
-//             controller: p.passwordController,
-//             decoration: InputDecoration(labelText: 'password'),
-//           ),
-//           SizedBox(
-//             height: 20,
-//           ),
-//           TextButton(
-//               onPressed: () async{
-//                 p.registerAndGetToken(context);
-//               },
-//               child: Text('submit'))
-//         ],
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/controller/register_controller.dart';
 import 'package:provider/provider.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({super.key});
+  const RegisterScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    final formkey = GlobalKey<FormState>();
-    final provide = Provider.of<RegisterController>(context, listen: false);
+    final formKey = GlobalKey<FormState>();
+    final provider = Provider.of<RegisterController>(context, listen: false);
 
     return Scaffold(
       body: Column(
@@ -61,7 +22,7 @@ class RegisterScreen extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Card(
                 child: Form(
-                  key: formkey,
+                  key: formKey,
                   child: Center(
                     child: Padding(
                       padding: const EdgeInsets.all(20),
@@ -69,65 +30,65 @@ class RegisterScreen extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           TextFormField(
-                            controller: provide.usernameController,
+                            controller: provider.usernameController,
                             decoration: InputDecoration(
                                 hintText: 'userName',
                                 border: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(20))),
                             validator: (value) {
                               if (value!.isEmpty) {
-                                return 'Please enter password';
+                                return 'Please enter a username';
                               } else {
                                 return null;
                               }
                             },
                           ),
-                          SizedBox(
+                          const SizedBox(
                             height: 20,
                           ),
                           TextFormField(
-                            controller: provide.emailController,
+                            controller: provider.emailController,
                             decoration: InputDecoration(
                                 hintText: 'email',
                                 border: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(20))),
                             validator: (value) {
                               if (value!.isEmpty) {
-                                return 'Please enter password';
+                                return 'Please enter an email';
                               } else {
                                 return null;
                               }
                             },
                           ),
-                          SizedBox(
+                          const SizedBox(
                             height: 20,
                           ),
                           TextFormField(
-                            controller: provide.passwordController,
+                            controller: provider.passwordController,
                             decoration: InputDecoration(
                                 hintText: 'password',
                                 border: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(20))),
                             validator: (value) {
                               if (value!.isEmpty) {
-                                return 'Please enter password';
+                                return 'Please enter a password';
                               } else {
                                 return null;
                               }
                             },
                           ),
-                          SizedBox(
+                          const SizedBox(
                             height: 10,
                           ),
                           ElevatedButton(
                               onPressed: () async {
-                                if (formkey.currentState!.validate()) {
-                                  await provide.registerAndGetToken(context);
+                                if (formKey.currentState!.validate()) {
+                                  await provider.registerAndGetToken(context);
                                 } else {
                                   print('empty value');
                                 }
                               },
-                              child: Text('register'))
+                              child: const Text('Register'))
                         ],
                       ),
                     ),
